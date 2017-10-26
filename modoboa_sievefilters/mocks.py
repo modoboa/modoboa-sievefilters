928478a481e7e023dfb0052eae902acaed610cd0
Antoine Nguyen <tonio@ngyn.org>
2017-10-26 18:21:49 +0200

    Added unit tests.

--- modoboa_sievefilters/mocks.py
+++ modoboa_sievefilters/mocks.py
@@ -1,14 +1,59 @@
 """Mock objects."""
 
 
+SAMPLE_SIEVE_SCRIPT = """require ["fileinto"];
+
+# Filter: test1
+if anyof (header :contains "To" "test1") {
+    fileinto "Test1";
+}
+
+# Filter: test2
+if anyof (header :contains "To" "test2") {
+    fileinto "Test2";
+}
+"""
+
+
 class ManagesieveClientMock(object):
     """Fake managesieve client."""
+
+    def __init__(self, *args, **kwargs):
+        self.scripts = {
+            "main_script": SAMPLE_SIEVE_SCRIPT,
+            "second_script": SAMPLE_SIEVE_SCRIPT
+        }
 
     def connect(self, *args, **kwargs):
         return True
 
+    def capability(self):
+        return """
+"IMPLEMENTATION" "Example1 ManageSieved v001"
+"VERSION" "1.0"
+"SASL" "PLAIN SCRAM-SHA-1 GSSAPI"
+"SIEVE" "fileinto vacation"
+"STARTTLS"
+"""
+
+    def havespace(self, name, size):
+        return True
+
     def listscripts(self):
         return ("main_script", ["second_script"])
+
+    def getscript(self, name):
+        return self.scripts.get(name)
+
+    def putscript(self, name, content):
+        self.scripts[name] = content
+        return True
+
+    def setactive(self, name):
+        return True
+
+    def deletescript(self, name):
+        return True
 
 
 class IMAP4Mock(object):
--- modoboa_sievefilters/tests.py
+++ modoboa_sievefilters/tests.py
@@ -45,3 +45,92 @@ class SieveFiltersTestCase(ModoTestCase):
         """Test index view."""
         response = self.client.get(reverse("modoboa_sievefilters:index"))
         self.assertContains(response, "main_script (active)")
+
+    def test_getfs(self):
+        """Test getfs view."""
+        response = self.ajax_get(
+            reverse("modoboa_sievefilters:fs_get", args=["main_script"]))
+        self.assertIn(
+            "/sfilters/main_script/editfilter/test1/", response["content"])
+
+    def test_toggle_filter_state(self):
+        """Test toggle_filter_state view."""
+        url = reverse("modoboa_sievefilters:filter_toggle_state",
+                      args=["main_script", "test1"])
+        response = self.ajax_get(url)
+        self.assertEqual(response["color"], "red")
+        response = self.ajax_get(url)
+        self.assertEqual(response["color"], "green")
+
+    def test_move_filter_up(self):
+        """Test move_filter_up view."""
+        url = reverse("modoboa_sievefilters:filter_move_up",
+                      args=["main_script", "test2"])
+        response = self.ajax_get(url)
+        pos1 = response["content"].find(
+            "/sfilters/main_script/editfilter/test2/")
+        pos2 = response["content"].find(
+            "/sfilters/main_script/editfilter/test1/")
+        self.assertTrue(pos1 < pos2)
+
+    def test_move_filter_down(self):
+        """Test move_filter_down view."""
+        url = reverse("modoboa_sievefilters:filter_move_down",
+                      args=["main_script", "test1"])
+        response = self.ajax_get(url)
+        pos1 = response["content"].find(
+            "/sfilters/main_script/editfilter/test2/")
+        pos2 = response["content"].find(
+            "/sfilters/main_script/editfilter/test1/")
+        self.assertTrue(pos1 < pos2)
+
+    def test_new_filter(self):
+        """Test new_filter view."""
+        url = reverse("modoboa_sievefilters:filter_add", args=["main_script"])
+        response = self.client.get(url)
+        self.assertContains(response, "New filter")
+
+    def test_remove_filter(self):
+        """Test removefilter view."""
+        url = reverse(
+            "modoboa_sievefilters:filter_delete",
+            args=["second_script", "test1"])
+        response = self.ajax_get(url)
+        self.assertEqual(response, "Filter removed")
+
+    def test_download_filters_set(self):
+        """Test download_filters_set view."""
+        url = reverse("modoboa_sievefilters:fs_download", args=["main_script"])
+        response = self.client.get(url)
+        self.assertEqual(response.content, mocks.SAMPLE_SIEVE_SCRIPT)
+
+    def test_activate_filters_set(self):
+        """Test activate_filters_set view."""
+        url = reverse(
+            "modoboa_sievefilters:fs_activate", args=["second_script"])
+        response = self.ajax_get(url)
+        self.assertEqual(response["respmsg"], "Filters set activated")
+
+    def test_remove_filters_set(self):
+        """Test remove_filters_set view."""
+        url = reverse(
+            "modoboa_sievefilters:fs_delete", args=["second_script"])
+        response = self.ajax_get(url)
+        self.assertEqual(response["respmsg"], "Filters set deleted")
+
+    def test_new_filters_set(self):
+        """Test new_filters_set view."""
+        url = reverse("modoboa_sievefilters:fs_add")
+        response = self.client.get(url)
+        self.assertContains(response, "Create a new filters set")
+
+        data = {"name": "new_script"}
+        response = self.ajax_post(url, data)
+        self.assertEqual(response["respmsg"], "Filters set created")
+
+    def test_savefs(self):
+        """Test savefs view."""
+        url = reverse("modoboa_sievefilters:fs_save", args=["new_script"])
+        data = {"scriptcontent": mocks.SAMPLE_SIEVE_SCRIPT}
+        response = self.ajax_post(url, data)
+        self.assertEqual(response["respmsg"], "Filters set saved")
